f7f9cd91717897a892a288674727df9fb68e1c60
Darukio <damian-200@live.com.ar>
2025-04-03 08:50:48 -0300

    perf: optimized get_devices_info and organize_devices_info feat: implemented Device object feat: added activate_all_devices method

--- business_logic/device_manager.py
+++ business_logic/device_manager.py
@@ -18,84 +18,53 @@
 """
 
 import logging
-import random
-import eventlet
 import os
 import configparser
-import time
-
 config = configparser.ConfigParser()
-from .connection_manager import ConnectionManager, ping_device
-from ..utils.errors import ConnectionFailedError, OutdatedTimeError
 from ..utils.file_manager import find_root_directory, load_from_file
+from .models.device import Device
+from ..utils.errors import BaseError
 
-def organize_device_info(line):
-    """Parses a line of text and returns a dictionary with device details."""
-    parts = line.strip().split(" - ")
+def organize_devices_info(line: str):
+    """Parses a line of text and returns a Device instance."""
+    parts: list[str] = line.strip().split(" - ")
     if len(parts) != 8:
         return None  # Invalid format, return None
-    
-    keys = ["district_name", "model_name", "point", "ip", "id", "communication", "battery", "active"]
-    return dict(zip(keys, parts))
+    return Device(
+        district_name=parts[0],
+        model_name=parts[1],
+        point=parts[2],
+        ip=parts[3],
+        id=parts[4],
+        communication=parts[5],
+        battery_failing=parts[6],
+        active=parts[7]
+    )
 
-def get_device_info():
-    """Loads device information from a file and returns a list of dictionaries."""
-    file_path = os.path.join(find_root_directory(), 'info_devices.txt')
-    return [device for data in load_from_file(file_path) if (device := organize_device_info(data))]
-
-def exponential_backoff(attempt):
-    wait_time = min(2 ** attempt + random.uniform(0, 3), 30)
-    time.sleep(wait_time)
-    
-def network_operation_with_retry(op, ip, port, communication, max_attempts=3, from_service=False):
-    config.read(os.path.join(find_root_directory(), 'config.ini'))
-    max_attempts = int(config['Network_config']['retry_connection'])
-    result = None
+def get_devices_info():
+    """Loads devices information from a file and returns a list of Device instances."""
+    file_path: str = os.path.join(find_root_directory(), 'info_devices.txt')
+    devices: list[str] = []
     try:
-        conn_manager = ConnectionManager(ip, port, communication, from_service=from_service)
+        devices = [device for data in load_from_file(file_path) if (device := organize_devices_info(data))]
     except Exception as e:
-        logging.error(str(e))
-    
-    for _ in range(max_attempts):
-        try:
-            logging.debug(f'{ip} CONECTANDO!')
-            if not conn_manager.is_connected():
-                start_time = time.time()
-                conn_manager.connect()
-                end_time = time.time()
-                logging.debug(f'{ip} CONECTADO! {end_time-start_time:.2f}')
-            if conn_manager.is_connected():
-                logging.debug(f'{ip} OPERACION DE RED!')
-                start_time = time.time()
-                if hasattr(conn_manager, op):
-                    func = getattr(conn_manager, op)
-                    result = func()
-                else:
-                    raise AttributeError(f"La funcion '{op}' no existe en ConnectionManager")
-                end_time = time.time()
-                logging.debug(f'{ip} FINALIZANDO OPERACION DE RED! {end_time-start_time:.2f}')
-                try:
-                    conn_manager.end_connection()
-                except Exception as e:
-                    pass
-                logging.debug(f'{ip} FINALIZADO!')
-                break
-        except OutdatedTimeError as e:
-            logging.error(str(e))
-            raise e
-        except ConnectionRefusedError as e:
-            conn_manager.reset_connection()
-            if result:
-                break
-            error_message = f"Intento fallido {_ + 1} de {max_attempts} del dispositivo {ip} para la operacion {op}: {e.__cause__}"
-            if _ + 1 == max_attempts:
-                raise ConnectionFailedError(error_message) from e
-            else:
-                ConnectionFailedError(error_message)
-            exponential_backoff(_)
-        except Exception as e:
-            logging.error(str(e))
-            pass
-                
-    logging.debug(f'{ip} RESULTADO!')
-    return result
+        raise BaseError(3001, str(e), level="critical")
+    return devices
+
+def activate_all_devices():
+    try:
+        with open('info_devices.txt', 'r') as file:
+            lines: list[str] = file.readlines()
+
+        new_lines: list[str] = []
+        for line in lines:
+            parts: list[str] = line.strip().split(' - ')
+            parts[7] = "True"
+            new_lines.append(' - '.join(parts) + '\n')
+
+        with open('info_devices.txt', 'w') as file:
+            file.writelines(new_lines)
+
+        logging.debug("Estado activo actualizado correctamente")
+    except Exception as e:
+        BaseError(3001, str(e))
